1e66ff5f36e4620c3d4f69b210dccc4163387ab0
Antoine <antoine.craipeau@efrei.net>
2023-12-22 11:32:46 +0100

    Output de stream

--- src/main/scala/CsvLoader.scala
+++ src/main/scala/CsvLoader.scala
@@ -3,37 +3,27 @@ import zio.stream._
 import zio.Console._
 import com.github.tototoshi.csv._
 
-implicit object CustomFormat extends DefaultCSVFormat { // Notre fichier CSV utilise le délimiteur ';' au lieu de ','
-  override val delimiter = ';'
-}
-
-def loadGasStationCsv():ZIO[Any & ZIOAppArgs & Scope, Throwable, Unit] = {
-  for {
-    _ <- printLine("Chargement du fichier CSV des stations essence")
-    url <- ZIO.succeed(getClass.getClassLoader.getResource("stations.csv"))
-    source <- ZIO.succeed(CSVReader.open(url.getFile))
-    stream <- ZStream
-      .fromIterator[Seq[String]](source.iterator)
-      .map[Option[GasStation]] {
-        case line if line.head == "id" => None
-        case line => Some(GasStation(
-          id = line.head.toInt,
-          population = parsePop(line(4)),
-          address = line(5),
-          city = line(6),
-          schedule = Some("Thinking about it"),
-          service = parseServices(line(26)),
-          automate24 = parseBool(line(25)),
-          region = Location.apply(line(30), line(29)),
-          department = Location.apply(line(28), line(27)),
-          gasList = parseGas(line(12), line(14), line(16), line(18), line(20), line(22)),
-          latitude = line(1).toDouble,
-          longitude = line(2).toDouble,
-        ))
-      }
-      .collectSome[GasStation]
-      .foreach(Console.printLine(_))
-  } yield ()
+def loadGasStationCsv(source: CSVReader): ZStream[Any, Any, GasStation]= {
+  ZStream
+    .fromIterator[Seq[String]](source.iterator)
+    .map[Option[GasStation]] {
+      case line if line.head == "id" => None
+      case line => Some(GasStation(
+        id = line.head.toInt,
+        population = parsePop(line(4)),
+        address = line(5),
+        city = line(6),
+        schedule = Some("Thinking about it"),
+        service = parseServices(line(26)),
+        automate24 = parseBool(line(25)),
+        region = Location(line(30), line(29)),
+        department = Location(line(28), line(27)),
+        gasList = parseGas(line(12), line(14), line(16), line(18), line(20), line(22)),
+        latitude = line(1).toDouble,
+        longitude = line(2).toDouble,
+      ))
+    }
+    .collectSome[GasStation]
 }
 
 def parsePop(s: String): Population =
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,16 +1,20 @@
 import zio._
 import zio.stream._
-import zio.http._
 import zio.Console._
+import com.github.tototoshi.csv._
+
+implicit object CustomFormat extends DefaultCSVFormat { // Notre fichier CSV utilise le délimiteur ';' au lieu de ','
+  override val delimiter = ';'
+}
 
 object App extends ZIOAppDefault {
 
   override def run: ZIO[Any & (ZIOAppArgs & Scope), Any, Unit] =
     for {
-      _ <- ZStream.fromIterable((1 to 10000).toList)
-        .take(10)
-        .map(_ * 2)
-        .foreach(Console.printLine(_))
+      _ <- printLine("Chargement du fichier CSV des stations essence")
+      url <- ZIO.succeed(getClass.getClassLoader.getResource("stations.csv"))
+      source <- ZIO.succeed(CSVReader.open(url.getFile))
+      stream <- loadGasStationCsv(source).foreach(printLine(_))
     } yield ()
 
 }
