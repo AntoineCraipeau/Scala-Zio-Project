a1a5dd6fdffa64621d2511388f97c0c7d1f39036
LeozeCroissant <leo.foulloy@gmail.com>
2023-12-30 16:04:19 +0100

    creating CLI for counting stations and average price

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -3,13 +3,10 @@ import zio.Console.*
 import com.github.tototoshi.csv.*
 import zio.stream.ZSink
 
-implicit object CustomFormat extends DefaultCSVFormat { // Notre fichier CSV utilise le délimiteur ';' au lieu de ','
-  override val delimiter = ';'
-}
-
-object App extends ZIOAppDefault {
-  override def run: ZIO[Any & (ZIOAppArgs & Scope), Any, Unit] =
+object Main extends ZIOAppDefault {
+  override def run: ZIO[Any & (ZIOAppArgs & Console), Any, Unit] =
     for {
-      stream <- makeTreatments()
+      _ <- printLine("Welcome to Gas Station Treatments!")
+      _ <- ConsoleApp.runMenu()
     } yield ()
 }
--- src/main/scala/Treatments.scala
+++ src/main/scala/Treatments.scala
@@ -4,23 +4,43 @@ import zio.Console.*
 import zio.ZIO
 import zio.stream.ZSink
 import GasStation.*
+import com.github.tototoshi.csv.DefaultCSVFormat
 
-def makeTreatments(): ZIO[Any, Any, Unit] ={
-  for{
-    _ <- printLine("Reading treatments")
-    count <- loadGasStationCsv()
-      .filter(_.geographicData.department.code == "75")
-      .filter(_.serviceData.automate24)
-      .tap(printLine(_))
-      .run(ZSink.count)
-    sum <- loadGasStationCsv()
-      .filter(_.geographicData.department.code == "75")
-      .filter(_.serviceData.automate24)
-      .map(_.serviceData.gasList(Gazol))
-      .map(GasPrice.unapply)
-      .collectSome[Double]
-      .run(ZSink.sum)
-    _ <- printLine(s"Number of matching stations: $count")
-    _ <- printLine(s"Average price: ${sum/count}")
-  } yield ()
+object Treatments{
+
+  implicit object CustomFormat extends DefaultCSVFormat {
+    override val delimiter = ';'
+  }
+
+  def countStations(): ZIO[Any, Any, Unit] ={
+    for{
+      _ <- printLine("Reading treatments")
+      count <- loadGasStationCsv()
+        .filter(_.geographicData.department.code == "75")
+        .filter(_.serviceData.automate24)
+        .tap(printLine(_))
+        .run(ZSink.count)
+      _ <- printLine(s"Number of matching stations: $count")
+    } yield ()
+  }
+
+  def averagePrice(): ZIO[Any, Any, Unit] ={
+    for{
+      _ <- printLine("Reading treatments")
+      count <- loadGasStationCsv()
+        .filter(_.geographicData.department.code == "75")
+        .tap(printLine(_))
+        .run(ZSink.count)
+      sum <- loadGasStationCsv()
+        .filter(_.geographicData.department.code == "75")
+        .map(_.serviceData.gasList(Gazol))
+        .map(GasPrice.unapply)
+        .collectSome[Double]
+        .run(ZSink.sum)
+      _ <- printLine(s"Average price of Gazol in Ile-de-France : ${sum/count}")
+    } yield ()
+  }
+
 }
+
+
