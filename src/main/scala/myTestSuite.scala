b816be5765b097e6b8e06de89fb8c476b7b8c6aa
KajikaSan <charlotte.bigare@gmail.com>
2024-01-11 10:39:30 +0100

    Implement test with zio test instend of munit

--- src/main/scala/myTestSuite.scala
+++
@@ -1,8 +0,0 @@
-import org.scalameta.munit.*
-
-class MyTestSuite extends FunSuite {
-  test("loadGasStationCsv should not return null") {
-    val result = loadGasStationCsv().runCollect.map(_.nonNull)
-    assert(result, "Gas stations loaded should not contain null values")
-  }
-}
